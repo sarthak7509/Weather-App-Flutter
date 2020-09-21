c6c104c0f7946668661a50daa484ce73d3e83bb6
sarthak7509 <60735499+sarthak7509@users.noreply.github.com>
2020-09-21 16:19:15 +0530

    Update admobservice.dart

--- lib/services/admobservice.dart
+++ lib/services/admobservice.dart
@@ -4,13 +4,13 @@ class AdMobService {
 
   String getAdMobAppId() {
     if (Platform.isAndroid) {
-      return "ca-app-pub-5580729396347961~7818326487";
+      return "{your andoid app Id}";
     }
   }
 
   String getBannerAdId() {
     if (Platform.isAndroid) {
-      return "ca-app-pub-5580729396347961/3108925289";
+      return "{your android banner id}";
     }
   }
 
